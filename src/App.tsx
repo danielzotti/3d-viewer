e929deb61b64012def50749858fd702f46d4d66b
Daniel Zotti <daniel.zotti87@gmail.com>
2022-08-05 17:54:36 +0200

    Various improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import type { Component } from 'solid-js';
-
 import styles from './App.module.scss';
 import { Viewer } from './components/viewer';
-import { createSignal } from 'solid-js';
-
-const getFormattedDate = (date?: Date) => new Intl.DateTimeFormat('it-IT', {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-}).format(date ? date : new Date());
 
 const App: Component = () => {
-
-  const [date, setDate] = createSignal<string>(getFormattedDate());
-
-  setInterval(() => setDate(getFormattedDate()), 1000);
-
   return (
     <div class={ styles.App }>
       <Viewer title="Mr Meeseeks" modelUrl={ 'src/assets/3d-models/mr-meeseks/scene.gltf' }/>
-      {/*<div>{ date() }</div>*/ }
     </div>
   );
 };
--- src/components/viewer/index.tsx
+++ src/components/viewer/index.tsx
@@ -1,15 +1,16 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import {
   AmbientLight, Box3,
-  Color,
-  DirectionalLight, Object3D,
-  PerspectiveCamera,
+  Color, DirectionalLight, DoubleSide, Mesh, MeshBasicMaterial, MeshStandardMaterial,
+  Object3D,
+  PerspectiveCamera, PlaneGeometry,
   PointLight, Renderer,
-  Scene, Vector3,
+  Scene, Texture, TextureLoader, Vector3,
   WebGLRenderer
 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './index.scss';
+import { createSignal } from 'solid-js';
 
 interface ViewerProps {
   title: string;
@@ -18,99 +19,125 @@ interface ViewerProps {
 
 export const Viewer = ({ title, modelUrl }: ViewerProps) => {
 
-  function fitCameraToObject(camera: PerspectiveCamera, object: Object3D, offset: number = 1.5) {
+    const [scene] = createSignal<Scene>(new Scene());
+    const [camera] = createSignal<PerspectiveCamera>(new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000));
+    const [renderer] = createSignal<Renderer>(new WebGLRenderer({ antialias: true }));
+    const [controls] = createSignal<OrbitControls>(new OrbitControls(camera(), renderer().domElement));
+    const [model, setModel] = createSignal<Object3D>();
 
-    const boundingBox = new Box3();
-
-    boundingBox.setFromObject(object);
-
-    const center = boundingBox.getCenter(new Vector3());
-    const size = boundingBox.getSize(new Vector3());
-
-    const startDistance = center.distanceTo(camera.position);
-    // here we must check if the screen is horizontal or vertical, because camera.fov is
-    // based on the vertical direction.
-    const endDistance = camera.aspect > 1 ?
-      ((size.y / 2) + offset) / Math.abs(Math.tan(camera.fov / 2)) :
-      ((size.y / 2) + offset) / Math.abs(Math.tan(camera.fov / 2)) / camera.aspect;
-
-
-    camera.position.set(
-      camera.position.x * endDistance / startDistance,
-      camera.position.y * endDistance / startDistance,
-      camera.position.z * endDistance / startDistance,
-    );
-    camera.lookAt(center);
-  }
-
-  function init() {
-    const scene = new Scene();
-    scene.background = new Color(0xdddddd);
-
-    const camera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
-    // camera.rotation.y = 45 / 180 * Math.PI;
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 1000;
-
-    const renderer: Renderer = new WebGLRenderer({ antialias: true });
-    renderer.setSize(parent.innerWidth, parent.innerHeight);
-    setTimeout(() => {
-      document.querySelector('#viewer')!.appendChild(renderer.domElement);
-    }, 0);
-
-    const animate = () => {
-      renderer.render(scene, camera);
+    function animate() {
+      renderer().render(scene(), camera());
       requestAnimationFrame(animate);
-    };
+      // console.log(camera().position)
+    }
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    const renderEventListener = () => {
-      renderer.render(scene, camera);
-    };
-    controls.addEventListener('change', renderEventListener);
+    // function onDocumentMouseDown(e: Event) {
+    //   console.log({ mouseDown: e });
+    // }
+    //
+    // function onDocumentMouseUp(e: Event) {
+    //   console.log({ mouseUp: e });
+    // }
+    //
+    // function onDocumentMouseMove(e: Event) {
+    //   console.log({ mouseMove: e });
+    // }
 
-    const ambientLight = new AmbientLight(0x404040, 3);
-    scene.add(ambientLight);
+    async function init() {
+      // SCENE
+      scene().background = new Color(0xdddddd);
 
-    const directionalLight = new DirectionalLight(0xffffff, 5);
-    directionalLight.position.set(0, 1, 0);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
-    const light = new PointLight(0xc4c4c4, .2);
-    light.position.set(0, 300, 500);
-    scene.add(light);
-    const light2 = new PointLight(0xc4c4c4, .2);
-    light2.position.set(500, 100, 0);
-    scene.add(light2);
-    const light3 = new PointLight(0xc4c4c4, .2);
-    light3.position.set(0, 100, -500);
-    scene.add(light3);
-    const light4 = new PointLight(0xc4c4c4, .2);
-    light4.position.set(-500, 300, 500);
-    scene.add(light4);
+      // PLANE
+      const groundMaterial = new TextureLoader().load('/src/assets/textures/ground-material.png');
+      const geometry = new PlaneGeometry(10, 10);
+      geometry.rotateX(Math.PI / 2);
+      geometry.rotateY(Math.PI / 2);
+      const material = new MeshStandardMaterial({ map: groundMaterial, side: DoubleSide });
+      const plane = new Mesh(geometry, material);
+      plane.receiveShadow = true;
+      scene().add(plane);
 
+      // LOAD OBJECT
+      let loader = new GLTFLoader();
+      const gltf = await loader.loadAsync(modelUrl);
+      setModel(gltf.scene.children[0]);
+      scene().add(gltf.scene);
 
-    let loader = new GLTFLoader();
-    loader.load(modelUrl, function(gltf) {
-      const myModel = gltf.scene.children[0];
-      // myModel.scale.set(10, 10, 10);
-      /*myModel.scale.set(10, 10, 10);
-      let box3 = new Box3().setFromObject(myModel);
-      let measure = new Vector3();
-      box3.getSize(measure);
-      console.log(measure);
-      myModel.position.y = -(window.innerHeight / 2) + measure.y;
-      */
-      fitCameraToObject(camera, myModel, 0);
-      scene.add(gltf.scene);
-      animate();
-    });
+      // RENDERER
+      renderer().setSize(parent.innerWidth, parent.innerHeight);
+
+      // LIGHTS
+      const ambientLight = new AmbientLight(0x404040, 3);
+      scene().add(ambientLight);
+
+      const directionalLight = new DirectionalLight(0xffffff, 5);
+      directionalLight.position.set(0, 0, 0);
+      directionalLight.castShadow = false;
+      scene().add(directionalLight);
+
+      const light = new PointLight(0xc4c4c4, 1);
+      light.position.set(300, 200, 300);
+      light.castShadow = true;
+      scene().add(light);
+
+      //CAMERA
+      // camera().rotation.y = 45 / 180 * Math.PI;
+      camera().position.set(1, 1, 10);
+      // camera().position.set(3.4, 24.9, 23.8);
+      fitCameraToObject();
+
+      // ADD TO DOM
+      document.querySelector('#viewer')!.appendChild(renderer().domElement);
+      animate(); // renderer().render(scene(), camera());
+
+      // LISTENERS
+      controls().addEventListener('change', () => {
+        renderer().render(scene(), camera());
+        console.log({ post: camera().position });
+      });
+
+      // document.addEventListener('mousedown', onDocumentMouseDown, false);
+      // document.addEventListener('mousemove', onDocumentMouseMove, false);
+      // document.addEventListener('mouseup', onDocumentMouseUp, false);
+
+    }
+
+    function fitCameraToObject(offset: number = 1.5) {
+      const object = model();
+      if(!object) {
+        console.error('No model');
+        return;
+      }
+
+      const boundingBox = new Box3();
+
+      boundingBox.setFromObject(object);
+
+      const center = boundingBox.getCenter(new Vector3());
+      const size = boundingBox.getSize(new Vector3());
+
+      const startDistance = center.distanceTo(camera().position);
+      // here we must check if the screen is horizontal or vertical, because camera().fov is
+      // based on the vertical direction.
+      const endDistance = camera().aspect > 1 ?
+        ((size.y / 2) + offset) / Math.abs(Math.tan(camera().fov / 2)) :
+        ((size.y / 2) + offset) / Math.abs(Math.tan(camera().fov / 2)) / camera().aspect;
+
+      const { x, y, z } = {
+        x: camera().position.x * endDistance / startDistance,
+        y: camera().position.y * endDistance / startDistance,
+        z: camera().position.z * endDistance / startDistance
+      };
+
+      camera().position.set(x, y, z);
+      camera().lookAt(camera().position);
+      // console.log({ fit: camera().position });
+    }
+
+    void init();
+
+    return <div id="viewer">
+      <h1>{ title }</h1>
+    </div>;
   }
-
-  init();
-
-  return <div id="viewer">
-    <h1>{ title }</h1>
-  </div>;
-};
+;
